6ab6b07f0cb1132f7479dc261933251296e0a735
Omkar Mamde <mamdeomkar23@gmail.com>
2026-05-07 16:43:00 +0530

    Delete model.py

--- model.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import login, upload_folder
-
-# (optional) Login with your Hugging Face credentials
-login()
-
-# Push your model files
-upload_folder(folder_path="C:\Text_Summerizer\saved_summary_model", repo_id="OmkarMamde/summerizer-t5", repo_type="model")
